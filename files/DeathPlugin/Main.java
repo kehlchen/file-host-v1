dc1263270c9f1869fc3b48dbd4afcfb1da31063d
kehlchen <kehlchenghg@gmail.com>
2026-06-09 17:53:21 +0200

    Aktualisieren von Main.java

--- files/DeathPlugin/Main.java
+++ files/DeathPlugin/Main.java
@@ -1,1 +1,31 @@
-yo
+package me.robin.deathplugin;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        // Dem Server sagen, dass dieses Plugin auf Events lauschen soll
+        Bukkit.getPluginManager().registerEvents(this, this);
+        getLogger().info("DeathPlugin erfolgreich aktiviert!");
+    }
+
+    @EventHandler
+    public void onPlayerDeath(PlayerDeathEvent event) {
+        // Den Spieler holen, der gerade gestorben ist
+        Player player = event.getEntity();
+        
+        // HIER den genauen Namen des Spielers eintragen (Groß-/Kleinschreibung ist egal)
+        if (player.getName().equalsIgnoreCase("Nebula888")) {
+            
+            // Die originale Todesnachricht mit deinem Text überschreiben
+            event.setDeathMessage(player.getName() + " is too Bad at the game");
+        }
+    }
+}
